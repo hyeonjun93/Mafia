a6a08f4d0fe1f27c035ac4159d517b69e9162e98
Alan Kim <hyeonjun93@hotmail.com>
2021-01-04 21:19:42 -0500

    Pushing latest

---
+++ Assets/PortalCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
